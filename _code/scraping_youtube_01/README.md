5b3eb63da12fe0e86cdc39ca422b0144b6be5c15
MarioX991 <kovacevicmarinko91@gmail.com>
2020-09-05 11:07:07 +0200

    finish youtube_scraping01

--- _code/scraping_youtube_01/README.md
+++ _code/scraping_youtube_01/README.md
@@ -1,5 +1,29 @@
 # Readme for code1.
 
+For this task, we scraped the website https://www.humblebundle.com/, but not the whole site just the book section. From which we gabbed information about book name, price of the book, and headers.
+
+
+The Scraped data has stored as a JSON file with the structure presented below:
+
+# data = {
+#     "header1":{
+#         "prince":200,
+#         "products":[
+#             "name1",
+#             "name2"
+#         ]
+#         },
+#         "headerw":{
+#         "prince":500,
+#         "products":[
+#             "name1",
+#             "name2"
+#         ]
+#     }
+#    .......
+
+# }
+
 
 #TODO
 1. we are capturing "SUPPORT CHARITY" by headline
@@ -11,24 +35,3 @@
 
 ## Activate it
     source venv/bin/activate
-
-### Data scture
-#data structure
-''' 
-lins = {
-    "line1":{
-        "prince":200,
-        "products":[
-            "name1",
-            "name2"
-        ]
-        },
-        "line2":{
-        "prince":500,
-        "products":[
-            "name1",
-            "name2"
-        ]
-    }
-}
-'''
--- _code/scraping_youtube_01/code1.py
+++ _code/scraping_youtube_01/code1.py
@@ -3,23 +3,51 @@
 import requests
 from bs4 import BeautifulSoup
 import pprint
+import json 
 
 url = 'https://www.humblebundle.com/books/programming-productivity-mercury-books'
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text, 'html.parser')
 
+# maybe better way of doing this is to try to get tada by the row
+
+tiers = soup.select('.dd-game-row')
+tier_dict ={}
+
+for tier in tiers:
+    #Only for section that have a headline
+    if tier.select(".dd-header-headline"):
+        #Grab tier name (and price)
+        tiername = tier.select('.dd-header-headline')[0].text.strip()
+        # Grab tier product names    
+        product_name = tier.select(".dd-image-box-caption")
+        product_name = [ prodname.text.strip().split("\n")[1] for prodname in product_name ]
+
+        #Add one product row to ur dictionary
+        tier_dict[tiername] ={"products" : product_name}
+
+
+with open("file.json","w") as f:
+    json.dump(tier_dict,f) 
+
+
+
 # headers above each book colection
 
-#this is one way to grab information but problem with this method is if in the future ownor deside to put another h2 tag into the current page,
+# this is one way to grab information but problem with this method is if in the future ownor deside to put another h2 tag into the current page,
 # this culd break our code for this reasone we need to do this with the class selector.
 # title_head = soup.find_all('h2')
 
-headline = soup.select('.dd-header-headline')
-dataHead = [line.text.strip() for line in headline]
+#headline = soup.select('.dd-header-headline')
+#dataHead = [line.text.strip() for line in headline]
+
+# Get cost for each books
+
+#Payment = [data.split(' ')[1] for data in dataHead if data.startswith('Pay')]
 
 # Products name 
-product_name = soup.select('.dd-image-box-caption')
-stripped_product_name  = [name.text.strip() for name in product_name]
+#product_name = soup.select('.dd-image-box-caption')
+# stripped_product_name  = [name.text.strip().split('\n')[1] for name in product_name]
 
 
 
@@ -27,7 +55,7 @@ stripped_product_name  = [name.text.strip() for name in product_name]
 
 #for lineName, lineinfo in dataHead.items():
 #    print(lineName)
-#    print("priced at", lineinfo["price"])
+#    print("priced at", ##########])
 #    print("priduct name: ", lineinfo["priducts"])
 
 
@@ -41,6 +69,47 @@ stripped_product_name  = [name.text.strip() for name in product_name]
 #  - product 1
 #  - product 2
 #  - product 4
+
+# lins = {
+#     "line1":{
+#         "prince":200,
+#         "products":[
+#             "name1",
+#             "name2"
+#         ]
+#         },
+#         "line2":{
+#         "prince":500,
+#         "products":[
+#             "name1",
+#             "name2"
+#         ]
+#     }
+# }
+'
+
+
+
+
+
+
+
+# as
+# da
+# sd
+# a
+# das
+# d
+# as
+# das
+# d
+# as
+# das
+# d
+
+
+
+
 
 
 
